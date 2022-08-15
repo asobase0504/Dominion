feb2f0f14dc4538409356fb355a84147fcfdd4f1
YudaKaito <211019@st.yoshida-g.ac.jp>
2022-08-15 17:28:42 +0900

    ブロックの余白を追加

--- color.cpp
+++ color.cpp
@@ -4,9 +4,9 @@
 // Author YudaKaito
 // 
 //=========================================
-//-----------------------------------------------------------------------------
+//-----------------------------------------
 // include
-//-----------------------------------------------------------------------------
+//-----------------------------------------
 #include "color.h"
 
 //-----------------------------------------
@@ -28,10 +28,20 @@ CColor::~CColor()
 //-----------------------------------------
 HRESULT CColor::Init()
 {
-	m_color.insert(std::make_pair("COLOR1", D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f)));
-	m_color.insert(std::make_pair("COLOR2", D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f)));
-	m_color.insert(std::make_pair("COLOR3", D3DXCOLOR(0.45f, 0.45f, 0.9f, 1.0f)));
+	//m_color.insert(std::make_pair("COLOR1", D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f)));
+	//m_color.insert(std::make_pair("COLOR2", D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f)));
+	//m_color.insert(std::make_pair("COLOR3", D3DXCOLOR(0.45f, 0.45f, 0.9f, 1.0f)));
+	//m_color.insert(std::make_pair("COLOR4", D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f)));
+
+	m_color.insert(std::make_pair("COLOR1", D3DXCOLOR(1.0f, 0.9f, 0.2f, 1.0f)));
+	m_color.insert(std::make_pair("COLOR2", D3DXCOLOR(0.8f, 0.2f, 1.0f, 1.0f)));
+	m_color.insert(std::make_pair("COLOR3", D3DXCOLOR(0.1f, 0.3f, 0.3f, 1.0f)));
 	m_color.insert(std::make_pair("COLOR4", D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f)));
+
+	//m_color.insert(std::make_pair("COLOR1", D3DXCOLOR(1.0f, 0.9f, 0.9f, 1.0f)));
+	//m_color.insert(std::make_pair("COLOR2", D3DXCOLOR(0.0f, 0.0f, 0.1f, 1.0f)));
+	//m_color.insert(std::make_pair("COLOR3", D3DXCOLOR(0.45f, 0.45f, 0.9f, 1.0f)));
+	//m_color.insert(std::make_pair("COLOR4", D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f)));
 	return S_OK;
 }
 
