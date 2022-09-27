6b2d2d3771dc861ed22ea89d17e14e0d464b5796
YudaKaito <211019@st.yoshida-g.ac.jp>
2022-09-27 12:32:40 +0900

    チュートリアルとステージの調整

--- tutorial.cpp
+++ tutorial.cpp
@@ -68,7 +68,7 @@ HRESULT CTutorial::Init()
 	}
 
 	// ステージ情報の取得
-	m_stageInfo = LoadJsonStage(L"data/FILE/STAGE/stage02.json")["STAGE"];
+	m_stageInfo = LoadJsonStage(L"data/FILE/STAGE/stage02.json").at("STAGE");
 
 	ResetStage();
 	return S_OK;
@@ -111,7 +111,7 @@ void CTutorial::Update()
 	// 全ての項目をクリアしてるか否か
 	for (int i = 0; i < (int)m_isSuccess.size(); i++)
 	{
-		if (!m_isSuccess[i])
+		if (!m_isSuccess.at(i))
 		{
 			isResetText = false;
 			break;
@@ -144,7 +144,9 @@ void CTutorial::Update()
 	CheckMission();
 	ResetText();
 
-	if (CInput::GetKey()->Trigger(KEY_BACK))
+	bool isKeybord = CInput::GetKey()->Trigger(DIK_BACKSPACE) && m_controllerIndex == -1;
+	bool isJoyPad = CInput::GetKey()->Trigger(JOYPAD_BACK, m_controllerIndex) && m_controllerIndex != -1;
+	if (isKeybord || isJoyPad)
 	{
 		CApplication::GetInstance()->SetMode(CApplication::MODE_TYPE::TITLE);
 	}
@@ -167,9 +169,9 @@ void CTutorial::Draw()
 	for (int i = 0; i < m_text.size(); i++)
 	{
 		RECT rect = { 0, 550 + 28 * i, CApplication::GetInstance()->SCREEN_WIDTH, CApplication::GetInstance()->SCREEN_HEIGHT };
-		wsprintf(str, _T(m_text[i].c_str()));
+		wsprintf(str, _T(m_text.at(i).c_str()));
 
-		m_font->DrawText(NULL, str, -1, &rect, DT_CENTER, m_isSuccess[i] ? CApplication::GetInstance()->GetColor(3) :CApplication::GetInstance()->GetColor(0));
+		m_font->DrawText(NULL, str, -1, &rect, DT_CENTER, m_isSuccess.at(i) ? CApplication::GetInstance()->GetColor(3) :CApplication::GetInstance()->GetColor(0));
 	}
 }
 
@@ -196,7 +198,7 @@ void CTutorial::ResetStage()
 	}
 
 	m_stage = new CStage;
-	m_stage->Init(m_stageInfo[m_stageIndex]);	// 初期化
+	m_stage->Init(m_stageInfo.at(m_stageIndex));	// 初期化
 }
 
 //-----------------------------------------------------------------------------
@@ -222,14 +224,14 @@ void CTutorial::ResetText()
 			m_textSubject = "移動";
 			m_text.resize(4);
 			m_isSuccess.resize(4);
-			m_isSuccess[0] = false;
-			m_isSuccess[1] = false;
-			m_isSuccess[2] = false;
-			m_isSuccess[3] = false;
-			m_text[0] = "・Ｗキーを押して上に移動\n";
-			m_text[1] = "・Ａキーを押して左に移動\n";
-			m_text[2] = "・Ｓキーを押して下に移動\n";
-			m_text[3] = "・Ｄキーを押して右に移動\n";
+			m_isSuccess.at(0) = false;
+			m_isSuccess.at(1) = false;
+			m_isSuccess.at(2) = false;
+			m_isSuccess.at(3) = false;
+			m_text.at(0) = "・Ｗキーを押して上に移動\n";
+			m_text.at(1) = "・Ａキーを押して左に移動\n";
+			m_text.at(2) = "・Ｓキーを押して下に移動\n";
+			m_text.at(3) = "・Ｄキーを押して右に移動\n";
 		}
 		break;
 		case CTutorial::SHOT:
@@ -237,14 +239,14 @@ void CTutorial::ResetText()
 			m_textSubject = "ショット";
 			m_text.resize(4);
 			m_isSuccess.resize(4);
-			m_isSuccess[0] = false;
-			m_isSuccess[1] = false;
-			m_isSuccess[2] = false;
-			m_isSuccess[3] = false;
-			m_text[0] = "・↑キーを押して上に撃つ\n";
-			m_text[1] = "・←キーを押して左に撃つ\n";
-			m_text[2] = "・↓キーを押して下に撃つ\n";
-			m_text[3] = "・→キーを押して右に撃つ\n";
+			m_isSuccess.at(0) = false;
+			m_isSuccess.at(1) = false;
+			m_isSuccess.at(2) = false;
+			m_isSuccess.at(3) = false;
+			m_text.at(0) = "・↑キーを押して上に撃つ\n";
+			m_text.at(1) = "・←キーを押して左に撃つ\n";
+			m_text.at(2) = "・↓キーを押して下に撃つ\n";
+			m_text.at(3) = "・→キーを押して右に撃つ\n";
 		}
 		break;
 		case CTutorial::CHARGE:
@@ -252,14 +254,14 @@ void CTutorial::ResetText()
 			m_textSubject = "チャージショット";
 			m_text.resize(4);
 			m_isSuccess.resize(4);
-			m_isSuccess[0] = false;
-			m_isSuccess[1] = false;
-			m_isSuccess[2] = false;
-			m_isSuccess[3] = false;
-			m_text[0] = "・↑キーを長押して上に撃つ\n";
-			m_text[1] = "・←キーを長押して左に撃つ\n";
-			m_text[2] = "・↓キーを長押して下に撃つ\n";
-			m_text[3] = "・→キーを長押して右に撃つ\n";
+			m_isSuccess.at(0) = false;
+			m_isSuccess.at(1) = false;
+			m_isSuccess.at(2) = false;
+			m_isSuccess.at(3) = false;
+			m_text.at(0) = "・↑キーを長押して上に撃つ\n";
+			m_text.at(1) = "・←キーを長押して左に撃つ\n";
+			m_text.at(2) = "・↓キーを長押して下に撃つ\n";
+			m_text.at(3) = "・→キーを長押して右に撃つ\n";
 		}
 		break;
 		default:
@@ -275,8 +277,8 @@ void CTutorial::ResetText()
 			m_textSubject = "移動";
 			m_text.resize(1);
 			m_isSuccess.resize(1);
-			m_isSuccess[0] = false;
-			m_text[0] = "・左スティックを動かして移動\n";
+			m_isSuccess.at(0) = false;
+			m_text.at(0) = "・左スティックを動かして移動\n";
 		}
 		break;
 		case CTutorial::SHOT:
@@ -284,14 +286,14 @@ void CTutorial::ResetText()
 			m_textSubject = "ショット";
 			m_text.resize(4);
 			m_isSuccess.resize(4);
-			m_isSuccess[0] = false;
-			m_isSuccess[1] = false;
-			m_isSuccess[2] = false;
-			m_isSuccess[3] = false;
-			m_text[0] = "・Ｘキーを押して上に撃つ\n";
-			m_text[1] = "・Ｙキーを押して左に撃つ\n";
-			m_text[2] = "・Ａキーを押して下に撃つ\n";
-			m_text[3] = "・Ｂキーを押して右に撃つ\n";
+			m_isSuccess.at(0) = false;
+			m_isSuccess.at(1) = false;
+			m_isSuccess.at(2) = false;
+			m_isSuccess.at(3) = false;
+			m_text.at(0) = "・Ｘキーを押して上に撃つ\n";
+			m_text.at(1) = "・Ｙキーを押して左に撃つ\n";
+			m_text.at(2) = "・Ａキーを押して下に撃つ\n";
+			m_text.at(3) = "・Ｂキーを押して右に撃つ\n";
 		}
 		break;
 		case CTutorial::CHARGE:
@@ -299,14 +301,14 @@ void CTutorial::ResetText()
 			m_textSubject = "チャージショット";
 			m_text.resize(4);
 			m_isSuccess.resize(4);
-			m_isSuccess[0] = false;
-			m_isSuccess[1] = false;
-			m_isSuccess[2] = false;
-			m_isSuccess[3] = false;
-			m_text[0] = "・Ｘキーを長押して上に撃つ\n";
-			m_text[1] = "・Ｙキーを長押して左に撃つ\n";
-			m_text[2] = "・Ａキーを長押して下に撃つ\n";
-			m_text[3] = "・Ｂキーを長押して右に撃つ\n";
+			m_isSuccess.at(0) = false;
+			m_isSuccess.at(1) = false;
+			m_isSuccess.at(2) = false;
+			m_isSuccess.at(3) = false;
+			m_text.at(0) = "・Ｘキーを長押して上に撃つ\n";
+			m_text.at(1) = "・Ｙキーを長押して左に撃つ\n";
+			m_text.at(2) = "・Ａキーを長押して下に撃つ\n";
+			m_text.at(3) = "・Ｂキーを長押して右に撃つ\n";
 		}
 		break;
 		default:
@@ -333,39 +335,39 @@ void CTutorial::CheckMission()
 		{
 			if (input->Press(DIK_W))
 			{
-				m_isSuccess[0] = true;
+				m_isSuccess.at(0) = true;
 			}
 			if (input->Press(DIK_A))
 			{
-				m_isSuccess[1] = true;
+				m_isSuccess.at(1) = true;
 			}
 			if (input->Press(DIK_S))
 			{
-				m_isSuccess[2] = true;
+				m_isSuccess.at(2) = true;
 			}
 			if (input->Press(DIK_D))
 			{
-				m_isSuccess[3] = true;
+				m_isSuccess.at(3) = true;
 			}
 		}
 		break;
 		case CTutorial::SHOT:
 		{
-			if (input->Press(DIK_UP))
+			if (input->Release(DIK_UP))
 			{
-				m_isSuccess[0] = true;
+				m_isSuccess.at(0) = true;
 			}
-			if (input->Press(DIK_LEFT))
+			if (input->Release(DIK_LEFT))
 			{
-				m_isSuccess[1] = true;
+				m_isSuccess.at(1) = true;
 			}
-			if (input->Press(DIK_DOWN))
+			if (input->Release(DIK_DOWN))
 			{
-				m_isSuccess[2] = true;
+				m_isSuccess.at(2) = true;
 			}
-			if (input->Press(DIK_RIGHT))
+			if (input->Release(DIK_RIGHT))
 			{
-				m_isSuccess[3] = true;
+				m_isSuccess.at(3) = true;
 			}
 		}
 		break;
@@ -383,7 +385,7 @@ void CTutorial::CheckMission()
 			{
 				if (chargeCount >= 50)
 				{
-					m_isSuccess[0] = true;
+					m_isSuccess.at(0) = true;
 					chargeCount = 0;
 				}
 				else
@@ -395,7 +397,7 @@ void CTutorial::CheckMission()
 			{
 				if (chargeCount >= 50)
 				{
-					m_isSuccess[1] = true;
+					m_isSuccess.at(1) = true;
 					chargeCount = 0;
 				}
 				else
@@ -407,7 +409,7 @@ void CTutorial::CheckMission()
 			{
 				if (chargeCount >= 50)
 				{
-					m_isSuccess[2] = true;
+					m_isSuccess.at(2) = true;
 					chargeCount = 0;
 				}
 				else
@@ -419,7 +421,7 @@ void CTutorial::CheckMission()
 			{
 				if (chargeCount >= 50)
 				{
-					m_isSuccess[3] = true;
+					m_isSuccess.at(3) = true;
 					chargeCount = 0;
 				}
 				else
@@ -439,41 +441,93 @@ void CTutorial::CheckMission()
 		{
 		case CTutorial::MOVE:
 		{
-			m_textSubject = "移動";
-			m_text.resize(1);
-			m_isSuccess.resize(1);
-			m_isSuccess[0] = false;
-			m_text[0] = "・左スティックを動かして移動\n";
+			if (input->Press(KEY_MOVE_UP, m_controllerIndex) ||
+				input->Press(KEY_MOVE_DOWN, m_controllerIndex) ||
+				input->Press(KEY_MOVE_LEFT, m_controllerIndex) ||
+				input->Press(KEY_MOVE_RIGHT, m_controllerIndex))
+			{
+				m_isSuccess.at(0) = true;
+			}
 		}
 		break;
 		case CTutorial::SHOT:
 		{
-			m_textSubject = "ショット";
-			m_text.resize(4);
-			m_isSuccess.resize(4);
-			m_isSuccess[0] = false;
-			m_isSuccess[1] = false;
-			m_isSuccess[2] = false;
-			m_isSuccess[3] = false;
-			m_text[0] = "・Ｘキーを押して上に撃つ\n";
-			m_text[1] = "・Ｙキーを押して左に撃つ\n";
-			m_text[2] = "・Ａキーを押して下に撃つ\n";
-			m_text[3] = "・Ｂキーを押して右に撃つ\n";
+			if (input->Release(KEY_SHOT_UP, m_controllerIndex))
+			{
+				m_isSuccess.at(0) = true;
+			}
+			if (input->Release(KEY_SHOT_LEFT, m_controllerIndex))
+			{
+				m_isSuccess.at(1) = true;
+			}
+			if (input->Release(KEY_SHOT_DOWN, m_controllerIndex))
+			{
+				m_isSuccess.at(2) = true;
+			}
+			if (input->Release(KEY_SHOT_RIGHT, m_controllerIndex))
+			{
+				m_isSuccess.at(3) = true;
+			}
 		}
 		break;
 		case CTutorial::CHARGE:
 		{
-			m_textSubject = "チャージショット";
-			m_text.resize(4);
-			m_isSuccess.resize(4);
-			m_isSuccess[0] = false;
-			m_isSuccess[1] = false;
-			m_isSuccess[2] = false;
-			m_isSuccess[3] = false;
-			m_text[0] = "・Ｘキーを長押して上に撃つ\n";
-			m_text[1] = "・Ｙキーを長押して左に撃つ\n";
-			m_text[2] = "・Ａキーを長押して下に撃つ\n";
-			m_text[3] = "・Ｂキーを長押して右に撃つ\n";
+			if (input->Press(KEY_SHOT_UP, m_controllerIndex) ||
+				input->Press(KEY_SHOT_LEFT, m_controllerIndex) ||
+				input->Press(KEY_SHOT_DOWN, m_controllerIndex) ||
+				input->Press(KEY_SHOT_RIGHT, m_controllerIndex))
+			{
+				chargeCount++;
+			}
+
+			if (input->Release(KEY_SHOT_UP, m_controllerIndex))
+			{
+				if (chargeCount >= 50)
+				{
+					m_isSuccess.at(0) = true;
+					chargeCount = 0;
+				}
+				else
+				{
+					chargeCount = 0;
+				}
+			}
+			if (input->Release(KEY_SHOT_LEFT, m_controllerIndex))
+			{
+				if (chargeCount >= 50)
+				{
+					m_isSuccess.at(1) = true;
+					chargeCount = 0;
+				}
+				else
+				{
+					chargeCount = 0;
+				}
+			}
+			if (input->Release(KEY_SHOT_DOWN, m_controllerIndex))
+			{
+				if (chargeCount >= 50)
+				{
+					m_isSuccess.at(2) = true;
+					chargeCount = 0;
+				}
+				else
+				{
+					chargeCount = 0;
+				}
+			}
+			if (input->Release(KEY_SHOT_RIGHT, m_controllerIndex))
+			{
+				if (chargeCount >= 50)
+				{
+					m_isSuccess.at(3) = true;
+					chargeCount = 0;
+				}
+				else
+				{
+					chargeCount = 0;
+				}
+			}
 		}
 		break;
 		default:
